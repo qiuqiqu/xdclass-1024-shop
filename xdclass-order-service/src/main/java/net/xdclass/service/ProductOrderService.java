374fb992bec8a9366ba502b7c92eaad33f9075d5
qiuqiqu <1961237983@qq.com>
2024-09-07 10:21:56 +0800

    订单接口骨架搭建

--- xdclass-order-service/src/main/java/net/xdclass/service/ProductOrderService.java
+++ xdclass-order-service/src/main/java/net/xdclass/service/ProductOrderService.java
@@ -1,5 +1,14 @@
 package net.xdclass.service;
 
 
+import net.xdclass.request.ConfirmOrderRequest;
+import net.xdclass.util.JsonData;
+
 public interface ProductOrderService {
+    /**
+     * 创建订单
+     * @param orderRequest
+     * @return
+     */
+    JsonData confirmOrder(ConfirmOrderRequest orderRequest);
 }
--- xdclass-order-service/src/main/java/net/xdclass/service/impl/ProductOrderServiceImpl.java
+++ xdclass-order-service/src/main/java/net/xdclass/service/impl/ProductOrderServiceImpl.java
@@ -1,11 +1,17 @@
 package net.xdclass.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
+import net.xdclass.request.ConfirmOrderRequest;
 import net.xdclass.service.ProductOrderService;
+import net.xdclass.util.JsonData;
 import org.springframework.stereotype.Service;
 
 
 @Service
 @Slf4j
 public class ProductOrderServiceImpl implements ProductOrderService {
+    @Override
+    public JsonData confirmOrder(ConfirmOrderRequest orderRequest) {
+        return null;
+    }
 }

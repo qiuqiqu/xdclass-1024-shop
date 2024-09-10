b278c3743d4012c5a9d0e7e9ceab4d2c676a5788
qiuqiqu <1961237983@qq.com>
2024-09-10 21:52:44 +0800

    下单获取收货地址

--- xdclass-order-service/src/main/java/net/xdclass/service/impl/ProductOrderServiceImpl.java
+++ xdclass-order-service/src/main/java/net/xdclass/service/impl/ProductOrderServiceImpl.java
@@ -1,14 +1,21 @@
 package net.xdclass.service.impl;
 
+import com.alibaba.fastjson.TypeReference;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
+import net.xdclass.enums.BizCodeEnum;
+import net.xdclass.exception.BizException;
+import net.xdclass.feign.UserFeignService;
+import net.xdclass.interceptor.LoginInterceptor;
 import net.xdclass.mapper.ProductOrderMapper;
+import net.xdclass.model.LoginUser;
 import net.xdclass.model.ProductOrderDO;
 import net.xdclass.request.ConfirmOrderRequest;
 import net.xdclass.service.ProductOrderService;
+import net.xdclass.util.CommonUtil;
 import net.xdclass.util.JsonData;
+import net.xdclass.vo.ProductOrderAddressVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.ParameterMapper;
 import org.springframework.stereotype.Service;
 
 
@@ -18,13 +25,16 @@ public class ProductOrderServiceImpl implements ProductOrderService {
     @Autowired
     ProductOrderMapper productOrderMapper;
 
+    @Autowired
+    UserFeignService userFeignService;
+
     /**
      * * 防重提交
      * * 用户微服务-确认收货地址
      * * 商品微服务-获取最新购物项和价格
      * * 订单验价
-     *   * 优惠券微服务-获取优惠券
-     *   * 验证价格
+     * * 优惠券微服务-获取优惠券
+     * * 验证价格
      * * 锁定优惠券
      * * 锁定商品库存
      * * 创建订单对象
@@ -37,20 +47,46 @@ public class ProductOrderServiceImpl implements ProductOrderService {
      */
     @Override
     public JsonData confirmOrder(ConfirmOrderRequest orderRequest) {
+        //获取当前登录用户
+        ThreadLocal<LoginUser> threadLocal = LoginInterceptor.threadLocal;
+        //生成订单号
+        String orderOutTradeNo = CommonUtil.getStringNumRandom(32);
+        //获取用户收货地址
+        ProductOrderAddressVO addressVO = this.getUserAddress(orderRequest.getAddressId());
+        log.info("收货地址信息:{}", addressVO);
         return null;
     }
 
     /**
+     * 获取收货详情地址
+     *
+     * @param addressId
+     * @return
+     */
+    private ProductOrderAddressVO getUserAddress(long addressId) {
+        JsonData addressData = userFeignService.detail(addressId);
+        if (addressData.getCode() != 0) {
+            log.error("获取收获地址失败,msg:{}", addressData);
+            throw new BizException(BizCodeEnum.ADDRESS_NO_EXITS);
+        }
+
+        ProductOrderAddressVO addressVO = addressData.getData(new TypeReference<>() {});
+
+        return addressVO;
+    }
+
+    /**
      * 根据订单号查询 订单状态
+     *
      * @param outTradeNo
      * @return
      */
     @Override
     public String queryProductOrderState(String outTradeNo) {
         ProductOrderDO productOrderDO = productOrderMapper.selectOne(new QueryWrapper<ProductOrderDO>().eq("out_trade_no", outTradeNo));
-        if(productOrderDO == null){
+        if (productOrderDO == null) {
             return "";
-        }else {
+        } else {
             return productOrderDO.getState();
         }
     }

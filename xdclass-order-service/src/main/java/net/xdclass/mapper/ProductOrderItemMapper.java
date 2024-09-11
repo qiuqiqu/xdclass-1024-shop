5464cabb2d283808a5896a5ecd6618123261c616
qiuqiqu <1961237983@qq.com>
2024-09-11 20:29:36 +0800

    订单微服务-创建商品订单和订单项模块开发

--- xdclass-order-service/src/main/java/net/xdclass/mapper/ProductOrderItemMapper.java
+++ xdclass-order-service/src/main/java/net/xdclass/mapper/ProductOrderItemMapper.java
@@ -3,8 +3,16 @@ package net.xdclass.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import net.xdclass.model.ProductOrderItemDO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface ProductOrderItemMapper extends BaseMapper<ProductOrderItemDO> {
 
+    /**
+     * 批量插入
+     * @param list
+     */
+    void insertBatch( @Param("orderItemList") List<ProductOrderItemDO> list);
 }
--- xdclass-order-service/src/main/java/net/xdclass/service/impl/ProductOrderServiceImpl.java
+++ xdclass-order-service/src/main/java/net/xdclass/service/impl/ProductOrderServiceImpl.java
@@ -1,18 +1,20 @@
 package net.xdclass.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
-import net.xdclass.enums.BizCodeEnum;
-import net.xdclass.enums.CouponStateEnum;
+import net.xdclass.enums.*;
 import net.xdclass.exception.BizException;
 import net.xdclass.feign.CouponFeignSerivce;
 import net.xdclass.feign.ProductFeignService;
 import net.xdclass.feign.UserFeignService;
 import net.xdclass.interceptor.LoginInterceptor;
+import net.xdclass.mapper.ProductOrderItemMapper;
 import net.xdclass.mapper.ProductOrderMapper;
 import net.xdclass.model.LoginUser;
 import net.xdclass.model.ProductOrderDO;
+import net.xdclass.model.ProductOrderItemDO;
 import net.xdclass.request.ConfirmOrderRequest;
 import net.xdclass.request.LockCouponRecordRequest;
 import net.xdclass.request.LockProductRequest;
@@ -28,6 +30,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -46,6 +49,9 @@ public class ProductOrderServiceImpl implements ProductOrderService {
 
     @Autowired
     CouponFeignSerivce couponFeignSerivce;
+
+    @Autowired
+    private ProductOrderItemMapper productOrderItemMapper;
 
     /**
      * * 防重提交
@@ -67,7 +73,7 @@ public class ProductOrderServiceImpl implements ProductOrderService {
     @Override
     public JsonData confirmOrder(ConfirmOrderRequest orderRequest) {
         //获取当前登录用户
-        ThreadLocal<LoginUser> threadLocal = LoginInterceptor.threadLocal;
+        LoginUser loginUser = LoginInterceptor.threadLocal.get();
         //生成订单号
         String orderOutTradeNo = CommonUtil.getStringNumRandom(32);
         //获取用户收货地址
@@ -80,24 +86,31 @@ public class ProductOrderServiceImpl implements ProductOrderService {
         //获取购物车的最新商品价格（也会清空对应的购物车商品）
         JsonData cartItemDate = productFeignService.confirmOrderCartItem(productIdList);
 
-        List<OrderItemVO> orderItemList  = cartItemDate.getData(new TypeReference<>(){});
-        log.info("获取的商品:{}",orderItemList);
-        if(orderItemList == null){
+        List<OrderItemVO> orderItemList = cartItemDate.getData(new TypeReference<>() {
+        });
+        log.info("获取的商品:{}", orderItemList);
+        if (orderItemList == null) {
             //购物车商品不存在
             throw new BizException(BizCodeEnum.ORDER_CONFIRM_CART_ITEM_NOT_EXIST);
         }
 
         //验证价格，减去商品优惠券
-        this.checkPrice(orderItemList,orderRequest);
+        this.checkPrice(orderItemList, orderRequest);
 
         //锁定优惠券
-        this.lockCouponRecords(orderRequest ,orderOutTradeNo );
+        this.lockCouponRecords(orderRequest, orderOutTradeNo);
 
         //锁定库存
-        this.lockProductStocks(orderItemList,orderOutTradeNo);
+        this.lockProductStocks(orderItemList, orderOutTradeNo);
 
 
-        //创建订单  TODO
+        //创建订单
+        ProductOrderDO productOrderDO = this.saveProductOrder(orderRequest, loginUser, orderOutTradeNo, addressVO);
+
+        //创建订单项
+        this.saveProductOrderItems(orderOutTradeNo, productOrderDO.getId(), orderItemList);
+
+        //发送延迟消息，用于自动关单 TODO
 
 
         //创建支付  TODO
@@ -105,13 +118,82 @@ public class ProductOrderServiceImpl implements ProductOrderService {
     }
 
     /**
+     * 新增订单项
+     *
+     * @param orderOutTradeNo
+     * @param orderId
+     * @param orderItemList
+     */
+    private void saveProductOrderItems(String orderOutTradeNo, Long orderId, List<OrderItemVO> orderItemList) {
+        List<ProductOrderItemDO> list = orderItemList.stream().map(obj -> {
+            ProductOrderItemDO itemDO = new ProductOrderItemDO();
+            itemDO.setBuyNum(obj.getBuyNum());
+            itemDO.setProductId(obj.getProductId());
+            itemDO.setProductImg(obj.getProductImg());
+            itemDO.setProductName(obj.getProductTitle());
+
+            itemDO.setOutTradeNo(orderOutTradeNo);
+            itemDO.setCreateTime(new Date());
+
+            //单价
+            itemDO.setAmount(obj.getAmount());
+            //总价
+            itemDO.setTotalAmount(obj.getTotalAmount());
+            itemDO.setProductOrderId(orderId);
+            return itemDO;
+
+        }).collect(Collectors.toList());
+
+        productOrderItemMapper.insertBatch(list);
+    }
+
+
+    /**
+     * 创建订单
+     *
+     * @param orderRequest
+     * @param loginUser
+     * @param orderOutTradeNo
+     * @param addressVO
+     */
+    private ProductOrderDO saveProductOrder(ConfirmOrderRequest orderRequest, LoginUser loginUser, String orderOutTradeNo, ProductOrderAddressVO addressVO) {
+
+        ProductOrderDO productOrderDO = new ProductOrderDO();
+        productOrderDO.setUserId(loginUser.getId());
+        productOrderDO.setHeadImg(loginUser.getHeadImg());
+        productOrderDO.setNickname(loginUser.getName());
+
+        productOrderDO.setOutTradeNo(orderOutTradeNo);
+        productOrderDO.setCreateTime(new Date());
+        productOrderDO.setDel(0);
+        productOrderDO.setOrderType(ProductOrderTypeEnum.DAILY.name());
+
+        //实际支付的价格
+        productOrderDO.setPayAmount(orderRequest.getRealPayAmount());
+
+        //总价，未使用优惠券的价格
+        productOrderDO.setTotalAmount(orderRequest.getTotalAmount());
+        productOrderDO.setState(ProductOrderStateEnum.NEW.name());
+        ProductOrderTypeEnum.valueOf(orderRequest.getPayType()).name();
+        productOrderDO.setPayType(ProductOrderPayTypeEnum.valueOf(orderRequest.getPayType()).name());
+
+        productOrderDO.setReceiverAddress(JSON.toJSONString(addressVO));
+
+        productOrderMapper.insert(productOrderDO);
+
+        return productOrderDO;
+
+    }
+
+    /**
      * 锁定优惠券
+     *
      * @param orderRequest
      * @param orderOutTradeNo
      */
     private void lockCouponRecords(ConfirmOrderRequest orderRequest, String orderOutTradeNo) {
         List<Long> lockCouponRecordIds = new ArrayList<>();
-        if(orderRequest.getCouponRecordId()>0){
+        if (orderRequest.getCouponRecordId() > 0) {
             lockCouponRecordIds.add(orderRequest.getCouponRecordId());
 
             LockCouponRecordRequest lockCouponRecordRequest = new LockCouponRecordRequest();
@@ -120,7 +202,7 @@ public class ProductOrderServiceImpl implements ProductOrderService {
 
             //发起锁定优惠券请求
             JsonData jsonData = couponFeignSerivce.lockCouponRecords(lockCouponRecordRequest);
-            if(jsonData.getCode()!=0){
+            if (jsonData.getCode() != 0) {
                 throw new BizException(BizCodeEnum.COUPON_RECORD_LOCK_FAIL);
             }
         }
@@ -130,12 +212,13 @@ public class ProductOrderServiceImpl implements ProductOrderService {
 
     /**
      * 锁定商品库存
+     *
      * @param orderItemList
      * @param orderOutTradeNo
      */
     private void lockProductStocks(List<OrderItemVO> orderItemList, String orderOutTradeNo) {
 
-        List<OrderItemRequest> itemRequestList = orderItemList.stream().map(obj->{
+        List<OrderItemRequest> itemRequestList = orderItemList.stream().map(obj -> {
 
             OrderItemRequest request = new OrderItemRequest();
             request.setBuyNum(obj.getBuyNum());
@@ -149,8 +232,8 @@ public class ProductOrderServiceImpl implements ProductOrderService {
         lockProductRequest.setOrderItemList(itemRequestList);
 
         JsonData jsonData = productFeignService.lockProductStock(lockProductRequest);
-        if(jsonData.getCode()!=0){
-            log.error("锁定商品库存失败：{}",lockProductRequest);
+        if (jsonData.getCode() != 0) {
+            log.error("锁定商品库存失败：{}", lockProductRequest);
             throw new BizException(BizCodeEnum.ORDER_CONFIRM_LOCK_PRODUCT_FAIL);
         }
     }
@@ -159,6 +242,7 @@ public class ProductOrderServiceImpl implements ProductOrderService {
      * 验证价格
      * 1）统计全部商品的价格
      * 2) 获取优惠券(判断是否满足优惠券的条件)，总价再减去优惠券的价格 就是 最终的价格
+     *
      * @param orderItemList
      * @param orderRequest
      */
@@ -166,7 +250,7 @@ public class ProductOrderServiceImpl implements ProductOrderService {
         //统计购买商品总价格
         BigDecimal realPayAmount = new BigDecimal("0");
         if (orderItemList != null) {
-            for(OrderItemVO orderItemVO : orderItemList){
+            for (OrderItemVO orderItemVO : orderItemList) {
                 BigDecimal itemRealPayAmount = orderItemVO.getTotalAmount();
                 realPayAmount = realPayAmount.add(itemRealPayAmount);
             }
@@ -176,50 +260,52 @@ public class ProductOrderServiceImpl implements ProductOrderService {
         CouponRecordVO couponRecordVO = getCartCouponRecord(orderRequest.getCouponRecordId());
 
         //计算购物车价格，是否满足优惠券满减条件
-        if(couponRecordVO!=null){
+        if (couponRecordVO != null) {
 
             //计算是否满足满减
-            if(realPayAmount.compareTo(couponRecordVO.getConditionPrice()) < 0){
+            if (realPayAmount.compareTo(couponRecordVO.getConditionPrice()) < 0) {
                 throw new BizException(BizCodeEnum.ORDER_CONFIRM_COUPON_FAIL);
             }
-            if(couponRecordVO.getPrice().compareTo(realPayAmount)>0){
+            if (couponRecordVO.getPrice().compareTo(realPayAmount) > 0) {
                 realPayAmount = BigDecimal.ZERO;
 
-            }else {
+            } else {
                 realPayAmount = realPayAmount.subtract(couponRecordVO.getPrice());
             }
 
         }
 
         // 前端传来的价格和后端的价格验价
-        if(realPayAmount.compareTo(orderRequest.getRealPayAmount()) !=0 ){
-            log.error("订单验价失败：{}",orderRequest);
+        if (realPayAmount.compareTo(orderRequest.getRealPayAmount()) != 0) {
+            log.error("订单验价失败：{}", orderRequest);
             throw new BizException(BizCodeEnum.ORDER_CONFIRM_PRICE_FAIL);
         }
     }
 
     /**
      * 获取优惠券
+     *
      * @param couponRecordId
      * @return
      */
     private CouponRecordVO getCartCouponRecord(Long couponRecordId) {
 
-        if(couponRecordId ==null || couponRecordId < 0){
+        if (couponRecordId == null || couponRecordId < 0) {
             return null;
         }
 
         JsonData couponData = couponFeignSerivce.findUserCouponRecordById(couponRecordId);
 
-        if(couponData.getCode()!=0){
+        if (couponData.getCode() != 0) {
             throw new BizException(BizCodeEnum.ORDER_CONFIRM_COUPON_FAIL);
         }
 
-        if(couponData.getCode()==0){
+        if (couponData.getCode() == 0) {
 
-            CouponRecordVO couponRecordVO = couponData.getData(new TypeReference<>(){});
+            CouponRecordVO couponRecordVO = couponData.getData(new TypeReference<>() {
+            });
 
-            if(!couponAvailable(couponRecordVO)){
+            if (!couponAvailable(couponRecordVO)) {
                 log.error("优惠券使用失败");
                 throw new BizException(BizCodeEnum.COUPON_UNAVAILABLE);
             }
@@ -231,16 +317,17 @@ public class ProductOrderServiceImpl implements ProductOrderService {
 
     /**
      * 判断优惠券是否可用
+     *
      * @param couponRecordVO
      * @return
      */
     private boolean couponAvailable(CouponRecordVO couponRecordVO) {
 
-        if(couponRecordVO.getUseState().equalsIgnoreCase(CouponStateEnum.NEW.name())){
+        if (couponRecordVO.getUseState().equalsIgnoreCase(CouponStateEnum.NEW.name())) {
             long currentTimestamp = CommonUtil.getCurrentTimestamp();
             long end = couponRecordVO.getEndTime().getTime();
             long start = couponRecordVO.getStartTime().getTime();
-            if(currentTimestamp>= start && currentTimestamp<=end){
+            if (currentTimestamp >= start && currentTimestamp <= end) {
                 return true;
             }
         }
@@ -260,7 +347,8 @@ public class ProductOrderServiceImpl implements ProductOrderService {
             throw new BizException(BizCodeEnum.ADDRESS_NO_EXITS);
         }
 
-        ProductOrderAddressVO addressVO = addressData.getData(new TypeReference<>() {});
+        ProductOrderAddressVO addressVO = addressData.getData(new TypeReference<>() {
+        });
 
         return addressVO;
     }
